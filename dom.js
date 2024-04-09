df2dff56cd5c2f912149269d8904f583277c6363
Moksh parashar <parasharm239@gmail.com>
2024-04-09 05:37:49 -0700

    .

---
+++ dom.js
@@ -0,0 +1,19 @@
+// const div = document.getElementsByClassName("container")[0];
+// const button = document.querySelectorAll("button")[0];
+
+// function clickHandler() {
+
+// div.insertAdjacentHTML("beforebegin", "<h1>beforebegin</h1>");
+// div.insertAdjacentHTML("beforeend", "<h1>beforeend</h1>");
+// div.insertAdjacentHTML("afterbegin", "<h1>afterbegin</h1>");
+// div.insertAdjacentHTML("afterend", "<h1>afterend</h1>");
+// }
+// button.addEventListener("click", clickHandler);
+
+// setInterval(() => {
+//   console.log("setInterval");
+// }, 3000);
+
+// setTimeout(() => {
+//   console.log("setTimeout");
+// }, 3000);
---
+++ new.js
@@ -0,0 +1,11 @@
+// const arr = [1, 2, 3, 4];
+
+// arr.splice(3, 1, 10);
+
+// console.log(arr);
+
+// const str = "Name";
+
+// const newStr = str.toLowerCase();
+
+// console.log(newStr);
